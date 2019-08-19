f136380722e9f44c0ead66df08ee28b7ba3c5f7c
ananchai phahupongsub <ananchai@leaderplanet.co.th>
2019-08-19 14:24:28 +0700

    draf project component

--- src/component/notification_right/index.js
+++ src/component/notification_right/index.js
@@ -1,18 +1,57 @@
 import React from 'react';
-import {Badge, Icon} from 'antd';
+import {Badge, Icon, Menu, List, Avatar, Dropdown} from 'antd';
 
-const NotificationRight = props => (
-  <div
-    style={{
-      display: 'flex',
-      alignItems: 'center',
-      marginRight: 5,
-      color: '#fff',
-    }}
-  >
-    <Badge count={props.topic || 0} showZero>
-      <Icon type="bell" style={{fontSize: '30px'}} />
-    </Badge>
-  </div>
-);
+const NotificationRight = props => {
+  const menu = (
+    <List
+      style={{width: ''}}
+      dataSource={[
+        {
+          name: 'Lily',
+        },
+        {
+          name: 'Lily',
+        },
+      ]}
+      bordered
+      renderItem={item => (
+        <List.Item
+          key={item.id}
+          actions={[
+            // <a onClick={this.showDrawer} key={`a-${item.id}`}>
+            <span>
+              View Profile
+            </span>,
+          ]}
+        >
+          <List.Item.Meta
+            avatar={
+              <Avatar src="https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png" />
+            }
+            title={<div>{item.name}</div>}
+            description={"teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"}
+          />
+          {item.name}
+        </List.Item>
+      )}
+    />
+  );
+  return (
+    <Dropdown overlay={menu} placement="bottomCenter" trigger={['click']}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          marginRight: 5,
+          color: '#fff',
+          cursor: 'pointer',
+        }}
+      >
+        <Badge count={props.topic || 0} showZero>
+          <Icon type="bell" style={{fontSize: '30px'}} />
+        </Badge>
+      </div>
+    </Dropdown>
+  );
+};
 export default NotificationRight;
--- src/component/testdrawer/index.js
+++ src/component/testdrawer/index.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import {Drawer, Button} from 'antd';
+import {Drawer} from 'antd';
 
-class Test extends React.Component {
-  state = {visible: false};
-
-  onClose = () => {
-    this.setState ({
-      visible: false,
-    });
-  };
-
-  render () {
-    return (
-      <div>
-        <Drawer
-          title="Basic Drawer"
-          placement="right"
-          closable={false}
-          onClose={this.props.onClose}
-          visible={this.props.visible}
-        >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-        </Drawer>
-      </div>
-    );
-  }
-}
+const Test = props => {
+  return (
+    <div>
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        closable={false}
+        onClose={props.onClose}
+        visible={props.visible}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </div>
+  );
+};
 export default Test;
